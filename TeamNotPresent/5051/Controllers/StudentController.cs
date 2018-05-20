ea18fd9600badf24c03124cef971e02fdeba2dde
KattyZhao <zhaos@seattleu.edu>
2018-05-20 08:59:01 -0700

    adjust student controller

--- TeamNotPresent/5051/Controllers/StudentController.cs
+++ TeamNotPresent/5051/Controllers/StudentController.cs
@@ -24,225 +24,43 @@ namespace _5051.Controllers
         /// <returns></returns>
         public ActionResult Index()
         {
+
             // Load the list of data into the StudentList
             var myDataList = StudentBackend.Index();
             var StudentViewModel = new StudentViewModel(myDataList);
-            return View(StudentViewModel);
+            //return View(StudentViewModel);
+            return View();
         }
 
-        /// <summary>
-        /// Read information on a single Student
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        // GET: Student/Details/5
-        public ActionResult Read(string id = null)
+
+       public ActionResult Home()
         {
-            var myDataStudent = StudentBackend.Read(id);
-            if (myDataStudent == null)
-            {
-                RedirectToAction("Error", "Home", "Invalid Record");
-            }
-
-
-            var myData = new StudentDisplayViewModel(myDataStudent);
-            if (myData== null)
-            {
-                RedirectToAction("Error", "Home", "Invalid Record");
-            }
-
-            return View(myData);
+            return View(); 
         }
 
-        /// <summary>
-        /// This opens up the make a new Student screen
-        /// </summary>
-        /// <returns></returns>
-        // GET: Student/Create
-        public ActionResult Create()
+        public ActionResult Profile()
         {
-            var myData = new StudentModel();
-            return View(myData);
+            return View();
         }
 
-        /// <summary>
-        /// Make a new Student sent in by the create Student screen
-        /// </summary>
-        /// <param name="collection"></param>
-        /// <returns></returns>
-        // POST: Student/Create
-        [HttpPost]
-        public ActionResult Create([Bind(Include=
-                                        "Id,"+
-                                        "Name,"+
-                                        "Description,"+
-                                        "Uri,"+
-                                        "AvatarId,"+
-                                        "Status,"+
-                                        "")] StudentModel data)
+        public ActionResult Logout()
         {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    if (data == null)
-                    {
-                        return RedirectToAction("Error", new { route = "Home", action = "Error" });
-                    }
-
-                    if (string.IsNullOrEmpty(data.Id))
-                    {
-                        return View(data);
-                    }
-
-                    StudentBackend.Create(data);
-
-                    return RedirectToAction("Index");
-                }
-
-                // Send back for edit
-                return View(data);
-            }
-            catch
-            {
-                return RedirectToAction("Error", new { route = "Home", action = "Error" });
-            }
+            return View();
         }
 
-        /// <summary>
-        /// This will show the details of the Student to update
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        // GET: Student/Edit/5
-        public ActionResult Update(string id = null)
+        public ActionResult PostCards()
         {
-            var myDataStudent = StudentBackend.Read(id);
-            if (myDataStudent == null)
-            {
-                RedirectToAction("Error", "Home", "Invalid Record");
-            }
-
-            var myData = new StudentDisplayViewModel(myDataStudent);
-            if (myData == null)
-            {
-                RedirectToAction("Error", "Home", "Invalid Record");
-            }
-            return View(myData);
+            return View();
         }
 
-        /// <summary>
-        /// This updates the Student based on the information posted from the udpate page
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        // POST: Student/Update/5
-        [HttpPost]
-        public ActionResult Update([Bind(Include=
-                                        "Id,"+
-                                        "Name,"+
-                                        "Description,"+
-                                        "Uri,"+
-                                        "AvatarId,"+
-                                        "AvatarLevel,"+
-                                        "Tokens,"+
-                                        "Status,"+
-                                        "")] StudentDisplayViewModel data)
+        public ActionResult Calendar()
         {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    if (data == null)
-                    {
-                        return RedirectToAction("Error", new { route = "Home", action = "Error" });
-                    }
-
-                    if (string.IsNullOrEmpty(data.Id))
-                    {
-                        return View(data);
-                    }
-
-                    var myDataStudent = new StudentModel(data);
-                    StudentBackend.Update(myDataStudent);
-
-                    return RedirectToAction("Index");
-                }
-
-                // Send back for edit
-                return View(data);
-            }
-            catch
-            {
-                return RedirectToAction("Error", new { route = "Home", action = "Error" });
-            }
+            return View();
         }
 
-        /// <summary>
-        /// This shows the Student info to be deleted
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        // GET: Student/Delete/5
-        public ActionResult Delete(string id = null)
-        {
-            var myDataStudent = StudentBackend.Read(id);
-            if (myDataStudent == null)
-            {
-                RedirectToAction("Error", "Home", "Invalid Record");
-            }
+        
 
-            var myData = new StudentDisplayViewModel(myDataStudent);
-            if (myData == null)
-            {
-                RedirectToAction("Error", "Home", "Invalid Record");
-            }
 
-            return View(myData);
-        }
 
-        /// <summary>
-        /// This deletes the Student sent up as a post from the Student delete page
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
-        // POST: Student/Delete/5
-        [HttpPost]
-        public ActionResult Delete([Bind(Include=
-                                        "Id,"+
-                                        "Name,"+
-                                        "Description,"+
-                                        "AvatarId,"+
-                                        "Uri,"+
-                                        "")] StudentDisplayViewModel data)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    if (data == null)
-                    {
-                        return RedirectToAction("Error", new { route = "Home", action = "Error" });
-                    }
-
-                    if (string.IsNullOrEmpty(data.Id))
-                    {
-                        return View(data);
-                    }
-
-                    StudentBackend.Delete(data.Id);
-
-                    return RedirectToAction("Index");
-                }
-
-                // Send back for edit
-                return View(data);
-            }
-            catch
-            {
-                return RedirectToAction("Error", new { route = "Home", action = "Error" });
-            }
-
-        }
     }
 }
