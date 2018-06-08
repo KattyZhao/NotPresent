192797e966940db94779a17496304bd8a4edd7ac
shivani <shivani patnani@LAPTOP-CD4SVPCH>
2018-06-07 22:12:48 -0700

    Moved css and Js

---
+++ TeamNotPresent/5051/Scripts/Kiosk.js
@@ -0,0 +1,13 @@
+﻿var dt = new Date();
+
+document.getElementById("demo").innerHTML = dt.toLocaleString();
+
+var myVar = setInterval(function () { myTimer() }, 1000);
+
+function myTimer() {
+
+    var d = new Date();
+
+    document.getElementById("demo").innerHTML = d.toLocaleString();
+
+}
