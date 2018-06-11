f2ebf39815e41c62502d7128de35b2517b6df3b5
shivani <shivani patnani@LAPTOP-CD4SVPCH>
2018-06-10 18:48:44 -0700

    Comments on Model

--- TeamNotPresent/5051/Models/AdminProfileModel.cs
+++ TeamNotPresent/5051/Models/AdminProfileModel.cs
@@ -13,30 +13,37 @@ namespace _5051.Models
     /// </summary>
     public class AdminProfileModel
     {
+        //Student ID  which will generated automatically
         [Display(Name = "Student ID", Description = "Student's School Id")]
         [Required(ErrorMessage = "Id is required")]
         public string Id { get; set; }
 
+        //Uri contains the Student's Picture
         [Display(Name = "Uri", Description = "Picture to Show")]
         [Required(ErrorMessage = "Picture is required")]
         public string Uri { get; set; }
 
+        //Name will the Student'NAme
         [Display(Name = "Name", Description = "Student Name")]
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
 
+        //Power ID will be the school's Id from a previoursly used system so that they can find the Student's previous ID from there 
         [Display(Name = "Power ID", Description = "Scholl's Previous System ID")]
         [Required(ErrorMessage = "PowerId is required")]
         public string PowerId { get; set; }
 
+        //Student Contact will consist of Student's Phone Number
         [Display(Name = "Student Contact", Description = "Personal Phone Number")]
         [Required(ErrorMessage = "Student Contact is required")]
         public string PersonalContact { get; set; }
 
+        //Guardian Contact will have Student's Guradian's Phone number
         [Display(Name = "Guardian Contact", Description = "Parent's Phone Number")]
         [Required(ErrorMessage = "Guardian Contact is required")]
         public string GuardianContact { get; set; }
 
+        //Address will have the Student's House Address
         [Display(Name = "Address", Description = "Current Address")]
         [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
